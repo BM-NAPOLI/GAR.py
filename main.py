4bf213587a6085f6ddce475f2ea5f6232256ff83
Si Rayan <ahmilano310@gmail.com>
2026-01-22 13:27:13 +0100

    First uppdate commit

--- main.py
+++ main.py
@@ -1,3 +1,39 @@
 import cv2
 
-print("OpenCV version:", cv2.__version__)
+#! Dowland l model dyal kaxf lwajh 
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+)
+
+#? X3EL DIK LKAMIRA
+cam = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cam.read()
+    if not ret:
+        break
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=5
+    )
+
+    for (x, y, w, h) in faces:
+        cv2.rectangle(
+            frame,
+            (x, y),
+            (x + w, y + h),
+            (0, 255, 0),
+            2
+        )
+
+    cv2.imshow("Face Detection", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cam.release()
+cv2.destroyAllWindows()
